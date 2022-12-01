ae74a77216cd37994fce5bb7aa9db8b09351b662
jahid oussama <jahid20oussama@gmail.com>
2022-12-01 09:27:15 +0100

    front end

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    screens:{
+      xs:'300px',
+      sm:'300px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
